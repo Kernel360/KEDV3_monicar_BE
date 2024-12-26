11f9fa1f19cdca61a4b780dee587a2046fd769de
Suxxxxhyun <qkrtngus116@naver.com>
2024-12-26 17:22:29 +0900

    chore: 사용하지 않는 어노테이션 제거

--- monicar-collector/src/main/java/org/collector/presentation/CycleInfoController.java
+++ monicar-collector/src/main/java/org/collector/presentation/CycleInfoController.java
@@ -8,12 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RequestMapping("/api/cycle-info")
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class CycleInfoController {
 
 	private final CycleInfoService cycleInfoService;
