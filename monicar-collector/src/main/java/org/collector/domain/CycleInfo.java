0cd51f58ddfdb5c6c5ee3bf4c973e5a42227d816
Suxxxxhyun <qkrtngus116@naver.com>
2024-12-27 14:59:26 +0900

    refactor: @Builder 적용

--- monicar-collector/src/main/java/org/collector/domain/CycleInfo.java
+++ monicar-collector/src/main/java/org/collector/domain/CycleInfo.java
@@ -5,11 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CycleInfo {
@@ -24,15 +26,4 @@ public class CycleInfo {
 	private String spd;
 	private String sum;
 	private String bat;
-
-	public CycleInfo(String sec, String gcd, String lat, String lon, String ang, String spd, String sum, String bat) {
-		this.sec = sec;
-		this.gcd = gcd;
-		this.lat = lat;
-		this.lon = lon;
-		this.ang = ang;
-		this.spd = spd;
-		this.sum = sum;
-		this.bat = bat;
-	}
 }
