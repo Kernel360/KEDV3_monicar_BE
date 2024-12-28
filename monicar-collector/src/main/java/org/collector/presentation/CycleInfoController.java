e638c49fa5f8a4727830770a7e36216bfe8105e5
Suxxxxhyun <qkrtngus116@naver.com>
2024-12-28 14:59:00 +0900

    feat: 예외처리 작업중

--- monicar-collector/src/main/java/org/collector/presentation/CycleInfoController.java
+++ monicar-collector/src/main/java/org/collector/presentation/CycleInfoController.java
@@ -1,6 +1,7 @@
 package org.collector.presentation;
 
 import org.collector.application.CycleInfoService;
+import org.collector.common.response.CommonResponse;
 import org.common.dto.CycleInfoRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,8 @@ public class CycleInfoController {
 	private final CycleInfoService cycleInfoService;
 
 	@PostMapping
-	public void cycleInfoSave(final @RequestBody CycleInfoRequest request) {
+	public CommonResponse<Void> cycleInfoSave(final @RequestBody CycleInfoRequest request) {
 		cycleInfoService.cycleInfoSave(request);
+		return CommonResponse.ok("111");
 	}
 }
