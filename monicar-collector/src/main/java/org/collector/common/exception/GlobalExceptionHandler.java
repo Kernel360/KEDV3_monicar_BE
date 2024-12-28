58996a94d8b88ad5eff4847c5ea354425eb3dbd6
Suxxxxhyun <qkrtngus116@naver.com>
2024-12-28 14:59:00 +0900

    feat: 예외처리 작업중

--- monicar-collector/src/main/java/org/collector/common/exception/GlobalExceptionHandler.java
+++ monicar-collector/src/main/java/org/collector/common/exception/GlobalExceptionHandler.java
@@ -1,24 +1,9 @@
 package org.collector.common.exception;
 
-import org.collector.common.response.CommonResponse;
-import org.collector.common.response.ResponseCode;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler{
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public CommonResponse<Object> handleMethodArgumentNotValid(
-		MethodArgumentNotValidException ex
-	) {
-		String errorMessage = ex.getBindingResult().getAllErrors()
-			.stream()
-			.map(ObjectError::getDefaultMessage)
-			.findFirst()
-			.orElse("Validation error");
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-		return CommonResponse.fail(new CustomException(ResponseCode.REQUIRED_PARAMETER_ERROR), errorMessage);
-	}
 }
